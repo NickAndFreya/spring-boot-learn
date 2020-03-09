e4dbd84d7e28c53987dac00bb9463f039e15a853
nick <nick_1106@163.com>
2020-03-09 22:03:06 +0800

    springboot集成mybatis-plus多数据源

---
+++ springboot-mybatis-plus-multi-datasource/src/main/java/com/freya/mybatis/plus/multi/SpringbootMybatisPlusMultiDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.freya.mybatis.plus.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlusMultiDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisPlusMultiDatasourceApplication.class, args);
+	}
+
+}
