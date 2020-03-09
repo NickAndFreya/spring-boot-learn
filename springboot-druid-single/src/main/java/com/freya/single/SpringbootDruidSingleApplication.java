62c6b63f75af6cdd6c919a7444e02520805bf8c6
nick <nick_1106@163.com>
2020-03-09 17:26:47 +0800

    springboot配置druid(单数据源)实现监控

---
+++ springboot-druid-single/src/main/java/com/freya/single/SpringbootDruidSingleApplication.java
@@ -0,0 +1,13 @@
+package com.freya.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDruidSingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDruidSingleApplication.class, args);
+	}
+
+}
