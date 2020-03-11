d6112ab4749b9b15713bb36183a7ce340e72086a
nick <nick_1106@163.com>
2020-03-11 12:08:46 +0800

    修改启动类名和module一致

--- springboot-mybatis-plus-dynamic-datasource/src/main/java/com/freya/mybatis/plus/multi/SpringbootMybatisPlusDynamicDatasourceApplication.java
+++ springboot-mybatis-plus-dynamic-datasource/src/main/java/com/freya/mybatis/plus/multi/SpringbootMybatisPlusDynamicDatasourceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootMybatisPlusMultiDatasourceApplication {
+public class SpringbootMybatisPlusDynamicDatasourceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMybatisPlusMultiDatasourceApplication.class, args);
+		SpringApplication.run(SpringbootMybatisPlusDynamicDatasourceApplication.class, args);
 	}
 
 }
