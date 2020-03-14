eb4632548f00b215596dc71fdf31ee4ab54737d1
nick <nick_1106@163.com>
2020-03-14 22:33:32 +0800

    接口增加注释

--- springboot-dubbo-server1/src/main/java/com/freya/dubbo/server1/controller/AnimalController.java
+++ springboot-dubbo-server1/src/main/java/com/freya/dubbo/server1/controller/AnimalController.java
@@ -14,6 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AnimalController {
 
+	/**
+	 * version:接口的版本管理，可以通过这个对一个接口进行多版本控制
+	 * cluster：集群容错，dubbo提供了五种集群容错模式，默认是failover，失败自动切换
+	 * check：启动的时候是否对服务进行检查，默认是true
+	 * async：是否支付异步调用，默认是false
+	 * timeout：接口的最大响应时间，消费端的优先级大于服务提供端的优先级
+	 * loadbalance：负载均衡模式，默认是随机模式
+	 */
+
 	@Reference(version = "${freya.server.version}", check = false, cluster = "failover", timeout = 5000, loadbalance = "random")
 	private AnimalService service;
 
