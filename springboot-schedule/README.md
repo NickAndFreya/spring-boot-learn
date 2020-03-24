ee98ff40ca40fb5ffe0218b6c3e83d73e212c46f
nick <nick_1106@163.com>
2020-03-24 10:56:41 +0800

    springboot @Scheduled 简单使用

---
+++ springboot-schedule/README.md
@@ -0,0 +1,4 @@
+## spring-boot @Scheduled 简单使用
+
+### 摘自
+[Java技术栈](https://mp.weixin.qq.com/s/e7wfN5M4zfo-x9utOnVv9w)
---
+++ springboot-schedule/src/main/java/com/freya/springboot/schedule/schedule/Scheduler.java
@@ -0,0 +1,37 @@
+package com.freya.springboot.schedule.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/**
+ * @author chengpiny
+ * @version 1.0.0
+ * @date 2020/3/24 10:40
+ */
+@Component
+@Slf4j
+public class Scheduler {
+	private static final LocalDateTime dateTime = LocalDateTime.now();
+
+	private static final Instant instant = Instant.now();
+
+	/***
+	 * 每隔2秒执行一次
+	 */
+	@Scheduled(fixedRate = 2000)
+	public void testTasks() {
+		log.info("固定频率定时任务执行时间:【{}】", instant);
+	}
+
+	/**
+	 * 每天10点50执行
+	 */
+	@Scheduled(cron = "0 50 10 ? * *")
+	public void testTask() {
+		log.info("CRON定时任务执行时间:【{}】", dateTime);
+	}
+}
