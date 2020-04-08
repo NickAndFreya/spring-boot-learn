0280f01d949d9046ea431b6407c04164d0d55096
nick <nick_1106@163.com>
2020-04-08 17:31:36 +0800

    tree init

---
+++ springboot-tree/src/main/java/com/freya/springboot/tree/ITree.java
@@ -0,0 +1,18 @@
+package com.freya.springboot.tree;
+
+/**
+ * @author chengpiny
+ * @version 1.0.0
+ * @date 2020/4/8 17:25
+ * @Description
+ */
+
+import java.util.List;
+
+public interface ITree {
+	public List<TreeNode> getTree();
+
+	public List<TreeNode> getRoot();
+
+	public TreeNode getTreeNode(String nodeId);
+}
---
+++ springboot-tree/src/main/java/com/freya/springboot/tree/ITreeNode.java
@@ -0,0 +1,15 @@
+package com.freya.springboot.tree;
+
+/**
+ * @author chengpiny
+ * @version 1.0.0
+ * @date 2020/4/8 17:24
+ * @Description
+ */
+public interface ITreeNode {
+	String getNodeId();
+	String getNodeName();
+	String getNodeParentId();
+	Integer getOrderNum();
+}
+
