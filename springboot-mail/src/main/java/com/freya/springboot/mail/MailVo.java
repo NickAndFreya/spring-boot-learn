c21d0b0506cc8c9ab1b498d113bb3725d9013d55
yuanchengpin <nick_1106@163.com>
2020-04-25 10:13:50 +0800

    springboot 集成 mail

---
+++ springboot-mail/src/main/java/com/freya/springboot/mail/MailVo.java
@@ -0,0 +1,24 @@
+package com.freya.springboot.mail;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Data
+public class MailVo {
+    private String id;
+    private String from;
+    private String to;
+    private String subject;
+    private String text;
+    private Date sentDate;
+    private String cc;
+    private String bcc;
+    private String status;
+    private String error;
+    @JsonIgnore
+    private MultipartFile[] multipartFiles;
+
+}
